1f914e69f7a2a51bf811c04d12f4d8cab01130e1
liebesu <lichunjiang42@gmail.com>
2016-02-01 09:22:28 +0800

    variable
    variable

--- bottle.py
+++ bottle.py
@@ -6,14 +6,14 @@ import sys
 
 
 def buy():
-    money=10
+    money=100
     price=2
     capchange=4
     bottlechange=2
     result=0
     bottle=0
     cap=0
-    for num in range(1,50):
+    for num in range(1,5000):
         print "第 " + str(num)+"操作"
         if money >= price :
             money=money-price
@@ -24,20 +24,20 @@ def buy():
             print "剩余钱 ："+str(money) +" 瓶子数量："+str(bottle)+"瓶盖数量："+str(cap)
 
         else:
-            if bottle>=2:
+            if bottle>=bottlechange:
                 result=result+1
                 bottle=bottle-1
                 cap=cap+1
                 print "瓶子换 1 瓶"
                 print "剩余钱 ："+str(money) +" 瓶子数量："+str(bottle)+"瓶盖数量："+str(cap)
             else:
-                if cap>=4:
+                if cap>=capchange:
                     result=result+1
                     bottle=bottle+1
                     cap=cap-3
                     print "瓶盖换 1 瓶"
                     print "剩余钱 ："+str(money) +" 瓶子数量："+str(bottle)+"瓶盖数量："+str(cap)
-        if money<price and bottle<2 and cap <4:
+        if money<price and bottle<bottlechange and cap <capchange:
             print "结果: "+str(result)
             print "剩余钱:"+str(money),"   剩余瓶子:"+str(bottle)," 剩余瓶盖:"+str(cap)
             sys.exit(0)
